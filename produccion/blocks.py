72083aa29d358586732b5df62ccb756c03bbff8e
Hernando Tejedor en arch <htejedor@gmail.com>
2026-02-16 22:02:49 -0300

    Agregado bloque ImageBlock y página Producto

--- produccion/blocks.py
+++ produccion/blocks.py
@@ -15,3 +15,17 @@ class ProductoBlock(StructBlock):
 
 class ProduccionStreamBlock(StreamBlock):
     producto = ProductoBlock(group="Section")
+
+
+class ImagenBlock(StructBlock):
+    nombre = CharBlock(required=False)
+    imagen = ImageBlock()
+    link = LinkBlock()
+
+    class Meta:
+        template = "produccion/blocks/imagen_block.html"
+
+
+class ImagenStreamBlock(StreamBlock):
+    imagen = ImagenBlock(group="Section")
+
--- produccion/models.py
+++ produccion/models.py
@@ -3,7 +3,7 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page
 
-from produccion.blocks import ProduccionStreamBlock
+from produccion.blocks import ProduccionStreamBlock, ImagenStreamBlock
 
 
 class AreaPage(Page):
@@ -20,4 +20,21 @@ class AreaPage(Page):
         FieldPanel("titulo"),
         FieldPanel("descripcion"),
         FieldPanel("productos"),
+    ]
+
+
+class ProductoPage(Page):
+    titulo = models.CharField(max_length=255)
+    descripcion = RichTextField(blank=True)
+    imagenes = StreamField(
+        ImagenStreamBlock(),
+        blank=True,
+        use_json_field=True,
+        help_text="Agregue una imagen y opcionalmente un nombre"
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("titulo"),
+        FieldPanel("descripcion"),
+        FieldPanel("imagenes"),
     ]
