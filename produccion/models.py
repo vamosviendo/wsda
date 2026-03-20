98c69bb5bd00205de65dfaf965d5ac5ea9a003be
Hernando Tejedor en arch <htejedor@gmail.com>
2026-03-20 10:12:18 -0300

    Agregada nueva página ElementoPage con detalles de los elementos de un producto

--- produccion/models.py
+++ produccion/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel
+import wagtail.blocks
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page
 
@@ -33,8 +34,94 @@ class ProductoPage(Page):
         help_text="Agregue una imagen y opcionalmente un nombre"
     )
 
+    subpage_types = ["produccion.ElementoPage"]
+
     content_panels = Page.content_panels + [
         FieldPanel("titulo"),
         FieldPanel("descripcion"),
         FieldPanel("imagenes"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["elementos"] = (
+            self.get_children()
+            .type(ElementoPage)
+            .live()
+            .specific()
+            .order_by("path")
+        )
+        return context
+
+
+class ElementoPage(Page):
+    imagen = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    alt_imagen = models.CharField(max_length=255, null=True, blank=True)
+    titulo = models.CharField(max_length=255, null=True, blank=True)
+    alto = models.DecimalField(
+        max_digits=8, decimal_places=2, null=True, blank=True,
+        verbose_name="Alto"
+    )
+    ancho = models.DecimalField(
+        max_digits=8, decimal_places=2, null=True, blank=True,
+        verbose_name="Ancho"
+    )
+    profundidad = models.DecimalField(
+        max_digits=8, decimal_places=2, null=True, blank=True,
+        verbose_name="Profundidad"
+    )
+    UNIDAD_CHOICES = [
+        ("cm", "cm"),
+        ("mm", "mm"),
+        ("m", "m"),
+    ]
+    unidad = models.CharField(
+        max_length=10, choices=UNIDAD_CHOICES, blank=True, default="cm",
+        verbose_name="Unidad de medida"
+    )
+    peso = models.DecimalField(
+        max_digits=8, decimal_places=2, null=True, blank=True,
+        verbose_name="Profundidad"
+    )
+    UNIDAD_PESO_CHOICES = [
+        ("g", "g"),
+        ("kg", "kg"),
+    ]
+    unidad_peso = models.CharField(
+        max_length=10, choices=UNIDAD_PESO_CHOICES, blank=True, default="kg",
+        verbose_name="Unidad de peso"
+    )
+    descripcion = RichTextField(blank=True)
+    comentarios = StreamField(
+        [("comentario", wagtail.blocks.RichTextBlock())],
+        blank=True,
+        use_json_field=True,
+    )
+
+    parent_page_types = ["produccion.ProductoPage"]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [FieldPanel("imagen"), FieldPanel("alt_imagen")],
+            heading="Imagen",
+        ),
+        FieldPanel("titulo"),
+        MultiFieldPanel(
+            [
+                FieldPanel("alto"),
+                FieldPanel("ancho"),
+                FieldPanel("profundidad"),
+                FieldPanel("unidad"),
+            ],
+            heading="Dimensiones",
+        ),
+        FieldPanel("peso"),
+        FieldPanel("descripcion"),
+        FieldPanel("comentarios"),
     ]
