c72d08a13a481b03f674aa5d29f9c2f4321d3006
Hernando Tejedor en arch <htejedor@gmail.com>
2026-04-06 19:28:36 -0300

    Aplicados estilos a página de contacto y landing

--- paginas/forms.py
+++ paginas/forms.py
@@ -2,7 +2,21 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    nombre = forms.CharField(label="Nombre", max_length=255)
-    email = forms.EmailField(label="Email")
-    asunto = forms.CharField(label="Asunto", max_length=255)
-    mensaje = forms.CharField(label="Mensaje", widget=forms.Textarea)
+    nombre = forms.CharField(
+        label="Nombre",
+        max_length=255,
+        widget=forms.TextInput(attrs={"class": "form-input"}),
+    )
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(attrs={"class": "form-input"}),
+    )
+    asunto = forms.CharField(
+        label="Asunto",
+        max_length=255,
+        widget=forms.TextInput(attrs={"class": "form-input"}),
+    )
+    mensaje = forms.CharField(
+        label="Mensaje",
+        widget=forms.Textarea(attrs={"class": "form-input"}),
+    )
