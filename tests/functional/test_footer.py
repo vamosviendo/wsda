ae30d3eba715a6bb8fe7eb8bacd1e003f800e2e2
Hernando Tejedor en arch <htejedor@gmail.com>
2026-06-21 10:21:33 -0300

    Functional test footer adaptado para trabajar con íconos de redes sociales en lugar de textos

--- tests/functional/test_footer.py
+++ tests/functional/test_footer.py
@@ -8,6 +8,10 @@ from base.models import NavigationSettings
 def navigation_settings():
     NavigationSettings(instagram_url="https://instagram.com/lilianamedela").save()
 
+
+def get_link_img_src(link):
+    return link.find_element(By.CSS_SELECTOR, "img").get_attribute("src")
+
 def test_footer(browser, site, navigation_settings):
     # En todas las páginas del sitio es visible el pie de página
     browser.get_page()
@@ -21,11 +25,12 @@ def test_footer(browser, site, navigation_settings):
     # Se muestran links configurados en NavigationSettings,
     # apuntando a las url correctas
     links = browser.find_elements(By.CSS_SELECTOR, "#footer-social a")
-    link_texts = [l.text.lower() for l in links]
-    assert "instagram" in link_texts
+    instagram_src = f"{browser.base_url}/static/images/instagram.svg"
+    link_srcs = [get_link_img_src(l) for l in links]
+    assert instagram_src in link_srcs
 
     insta_link = next(
-        (l for l in links if l.text.lower() == "instagram"),
+        (l for l in links if get_link_img_src(l) == instagram_src),
         None
     )
     assert insta_link is not None
